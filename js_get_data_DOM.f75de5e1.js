3b1f4e576470f8664ea222af706a341137bf5c00
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-10-29 15:12:49 +0200

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,20 @@
+'use strict';
+// write your code here
+const spanValue = [];
+const spans = document.getElementsByClassName('population');
+for (const span of spans){
+    const value = span.textContent;
+    const valueNumber = value.split(',').join('');
+    if (!isNaN(valueNumber) && valueNumber !== '') {
+        const number = Number(valueNumber);
+        spanValue.push(number);
+    }
+}
+const total = spanValue.reduce((prevValue, currentValue)=>prevValue + currentValue, 0);
+const average = Math.round(total / spanValue.length);
+const elementTotal = document.querySelector('.total-population');
+const elementAverage = document.querySelector('.average-population');
+elementTotal.textContent = total.toLocaleString('en-US');
+elementAverage.textContent = average.toLocaleString('en-US');
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
