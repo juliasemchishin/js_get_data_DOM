0b230e7c9c49769811cb9c3ec95479b2bfbc6e43
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-10-29 14:50:23 +0200

    add fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,5 +23,5 @@ const average = Math.round(total / spanValue.length);
 const elementTotal = document.querySelector('.total-population');
 const elementAverage = document.querySelector('.average-population');
 
-elementTotal.textContent = total.toLocaleString('en-Us');
-elementAverage.textContent = average.toLocaleString('en-Us');
+elementTotal.textContent = total.toLocaleString('en-US');
+elementAverage.textContent = average.toLocaleString('en-US');
