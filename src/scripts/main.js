98a331880b42643ccdcc7546e4dfd7fd74d27fd1
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-10-29 14:45:52 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+const spanValue = [];
+
+const spans = document.getElementsByClassName('population');
+
+for (const span of spans) {
+  const value = span.textContent;
+
+  const valueNumber = value.split(',').join('');
+
+  spanValue.push(+valueNumber);
+}
+
+const total = spanValue.reduce(
+  (prevValue, currentValue) => prevValue + currentValue,
+  0,
+);
+
+const average = Math.round(total / spanValue.length);
+
+const elementTotal = document.querySelector('.total-population');
+const elementAverage = document.querySelector('.average-population');
+
+elementTotal.textContent = total.toLocaleString('en-Us');
+elementAverage.textContent = average.toLocaleString('en-Us');
